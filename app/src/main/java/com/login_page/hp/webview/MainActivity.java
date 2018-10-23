0b76a6bb2f10ac98c3cb6dcf41ab36649f46b3f2
Dona Paulson <donavpaulson@gmail.com>
2018-10-23 16:10:47 +0530

    Initial commit

---
+++ app/src/main/java/com/login_page/hp/webview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.login_page.hp.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webview = findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://www.google.com");
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webview.canGoBack()){
+            webview.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+}
